acaea351dc371fa3ff464e7e5d3e89d44c2fa506
Pochelyk <Pochelyk@mail.ru>
2017-09-22 02:29:48 +0300

    Add files via upload

---
+++ WorkLoad/WorkLoad/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkLoad
+{
+    class Force
+    {
+        public double f;
+        public double L;
+        public double L1;
+        public double L2;
+        public double L3;
+        public double L4;
+        public double L5;
+        public double q1;
+        //public double m;
+        public double F()
+        {
+            return f * (L - L2);
+        }
+        public double Q()
+        {
+            return (q1 * L5) * ((L4 + L5 / 2) - L);
+        }
+        public double F1()
+        {
+            return f * ((L - L2) + (L1 - L));
+        }
+        public double Q1()
+        {
+            return (q1 * L5) * ((L4 + L5 / 2) - L1);
+        }
+    }
+}
---
+++ WorkLoad/WorkLoad/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkLoad
+{
+    class Program
+    {
+        static double ReadDouble(string message)
+        {
+            double result;
+            bool parsed;
+            do
+            {
+                Console.WriteLine(message);
+                string d = Console.ReadLine();
+                parsed = double.TryParse(d, out result);
+            } while (!parsed);
+            return result;
+        }
+        static Force MakeQ()
+        {
+            Force Q = new Force();
+            Q.q1 = ReadDouble("Введите силу распределённой нагрузки Q:");
+            Q.L4 = ReadDouble("Введите расстояние от начала отрезка:");
+            Q.L5 = ReadDouble("Введите длинну действия нагрузки:");
+            return Q;
+        }
+        static Force MakeF()
+        {
+            Force F = new Force();
+            F.f = ReadDouble("Введите силу постоянной нагрузки F=кН:");
+            F.L2 = ReadDouble("Введите расстояние от начала отрезка:");
+            return F;
+        }
+        static Force MakeForce()
+        {
+            Force force;
+            Console.WriteLine("Введите тип нагрузки F/Q/M");
+            string size = Console.ReadLine();
+            switch (size)
+            {
+                case "F":
+                    force = MakeF();
+                    break;
+                case "Q":
+                    force = MakeQ();
+                    break;
+                default:
+                    force = null;
+                    Console.WriteLine("Вы ввели неверные данные");
+                    break;
+            }
+            return force;
+        }
+        static void Main(string[] args)
+        {
+            double Stem = ReadDouble("Введите длину отрезка:");
+
+            Console.WriteLine("Введите тип левого защемления: П-подвижная опора; Н-неподвижная опора");
+            string Left = Console.ReadLine();
+            double L = ReadDouble("Введите расстояние от начала отрезка до опоры:");
+
+            Console.WriteLine("Введите тип правого защемления: П-подвижная опора; Н-неподвижная опора");
+            string Right = Console.ReadLine();
+            double L1 = ReadDouble("Введите расстояние от начала отрезка до опоры:");
+
+            Console.WriteLine("Введите количество действующих сил:");
+            string x = Console.ReadLine();
+            int X = Convert.ToInt32(x);
+
+            Force[] forces = new Force[X];
+            for (int i = 0; i < forces.Length; i++)
+            {
+                Force force = null;
+                do
+                {
+                    force = MakeForce();
+                } while (force == null);
+
+                forces[i] = force;
+            }
+            Console.ReadKey();
+        }
+    }
+}
