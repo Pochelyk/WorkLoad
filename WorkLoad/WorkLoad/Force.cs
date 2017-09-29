168eace1e0336cea4b941da1e197d5d99363535c
Pochelyk <Pochelyk@mail.ru>
2017-09-29 14:06:18 +0300

    Add files via upload

---
+++ WorkLoad/WorkLoad/Force.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkLoad
+{
+    public class LongBalk
+    {
+        public double Length;
+    }
+
+    // Pillar*Pillar*Pillar*Pillar*Pillar*Pillar*Pillar
+
+    class Force
+    {
+        public double Value;
+        public double Angle;
+    }
+    abstract class Pillar
+    {
+        public Force Reaction;
+        public double Distance;
+    }
+    class FixedPillar : Pillar
+    {
+    }
+    class FloatingPillar : Pillar
+    {
+    }
+    enum Direction
+    {
+        left,
+        right
+    }
+    class Moment
+    {
+        public double Value;
+        Direction Sign;
+    }
+    class Anchorage : Pillar
+    {
+        public Moment Bending;
+    }
+    enum Direct
+    {
+        top,
+        bottom
+    }
+
+    //Loading*Loading*Loading*Loading*Loading*Loading
+
+    abstract class Loading
+    {
+        public double Distance;
+    }
+    class UniformlyDistributed : Loading
+    {
+        public double value;
+        public double length;
+        Direct Sign;
+    }
+    class TransverseLoad : Loading
+    {
+        public Force Load;
+    }
+    class MomentLoad : Loading
+    {
+        public Moment Load;
+    }
+
+    class Model
+    {
+        public LongBalk Balka;
+
+        public Pillar[] Pillars;
+        public Loading[] Loadings;
+    }
+   
+}
--- WorkLoad/WorkLoad/Program.cs
+++ WorkLoad/WorkLoad/Program.cs
@@ -20,66 +20,53 @@ namespace WorkLoad
             } while (!parsed);
             return result;
         }
-        static Force MakeQ()
+        static LongBalk MaKeLength()
         {
-            Force Q = new Force();
-            Q.q1 = ReadDouble("Введите силу распределённой нагрузки Q:");
-            Q.L4 = ReadDouble("Введите расстояние от начала отрезка:");
-            Q.L5 = ReadDouble("Введите длинну действия нагрузки:");
-            return Q;
+            LongBalk longBalk = new LongBalk();
+            longBalk.Length = ReadDouble("Введите длину балки");
+            return longBalk;
         }
-        static Force MakeF()
-        {
-            Force F = new Force();
-            F.f = ReadDouble("Введите силу постоянной нагрузки F=кН:");
-            F.L2 = ReadDouble("Введите расстояние от начала отрезка:");
-            return F;
-        }       
         static Force MakeForce()
         {
-            Force force;
-            Console.WriteLine("Введите тип нагрузки F/Q/M");
-            string size = Console.ReadLine();
-            switch (size)
-            {
-                case "F":
-                    force = MakeF();
-                    break;
-                case "Q":
-                    force = MakeQ();
-                    break;
-                default:
-                    force = null;
-                    Console.WriteLine("Вы ввели неверные данные");
-                    break;
-            }
+            Force force = new Force();
+            force.Value = ReadDouble("Введите значение");
+            force.Angle = ReadDouble("Введите угол");
             return force;
         }
-        static void Main(string[] args)
+        static Moment MakeMoment()
         {
-            Console.WriteLine("Введите тип левого защемления: П-подвижная опора; Н-неподвижная опора");
-            string Left = Console.ReadLine();
-            double L = ReadDouble("Введите расстояние от начала отрезка до опоры:");
-
-            Console.WriteLine("Введите тип правого защемления: П-подвижная опора; Н-неподвижная опора");
-            string Right = Console.ReadLine();
-            double L1 = ReadDouble("Введите расстояние от начала отрезка до опоры:");
-
-            Console.WriteLine("Введите количество действующих сил:");
-            string x = Console.ReadLine();
-            int X = Convert.ToInt32(x);
-
-            Force[] forces = new Force[X];
-            for (int i = 0; i < forces.Length; i++)
-            {
-                Force force = null;
-                do
-                {
-                 force = MakeForce();
-                } while (force == null);
-                forces[i] = force;
-            }
-             Console.ReadKey();
+            Moment moment = new Moment();
+            moment.Value = ReadDouble("Введите значение");
+            return moment;
         }
+
+        static void FixedPillar(Pillar pillar)
+        {
+            pillar.Distance = ReadDouble("Введите рассстояние ");
+        }
+        static void FloatingPillar(Pillar pillar)
+        {
+            pillar.Distance = ReadDouble("Введите рассстояние ");
+        }
+        static void Anchorage(Pillar pillar)
+        {
+            pillar.Distance = ReadDouble("Введите рассстояние ");
+        }
+
+
+
+        static void UniformlyDistributed(Loading loading)
+        {
+            loading.Distance = ReadDouble("Введите значение");
+        }
+        static void TransverseLoad(Loading loading)
+        {
+            loading.Distance = ReadDouble("Введите значение");
+        }
+        static void MomentLoad(Loading loading)
+        {
+            loading.Distance = ReadDouble("Введите значение");
+        }
+
     }
 }
